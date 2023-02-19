0e1001d37c158be241b97dc5450ac343aa8d4f18
collinsregs <72102600+collinsregs@users.noreply.github.com>
2023-02-19 23:00:14 +0300

    homepage minor changes

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,10 +15,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    print(FirebaseAuth.instance.currentUser);
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 240, 235, 235),
       appBar: AppBar(
         backgroundColor: Color(0xFF4285f4),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'notifications',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text('Please verify your email'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          // ListView.builder(
+
+          // )
+        ],
       ),
     );
   }
